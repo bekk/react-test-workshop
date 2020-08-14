a41e48d5be7636b01af94e73ce29e6eabc2f185a
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-08-14 23:21:16 +0200

    Remove CRA setup we don't need and start making the todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Simple to-do list</h1>
+      Add things that you need to do here, and then remove them when you've
+      solved them!
+      <h2>List:</h2>
+      <ul>
+        <li>item 1...</li>
+        <li>item 2...</li>
+        <li>item 3...</li>
+        <li>item 4...</li>
+      </ul>
+      <h2>Add item</h2>
+      <input type="text" /> <button>Add</button>
+    </>
   );
 }
 
