f54841ceb999e4b694477e4f9a6e3b856222003e
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-08-14 23:27:55 +0200

    Save todo-list state with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState<string[]>([
+    "item1...",
+    "item2...",
+    "item3...",
+    "item4...",
+  ]);
+
   return (
     <>
       <h1>Simple to-do list</h1>
@@ -8,13 +15,15 @@ function App() {
       solved them!
       <h2>List:</h2>
       <ul>
-        <li>item 1...</li>
-        <li>item 2...</li>
-        <li>item 3...</li>
-        <li>item 4...</li>
+        {list.map((task) => (
+          <li>{task}</li>
+        ))}
       </ul>
-      <h2>Add item</h2>
-      <input type="text" /> <button>Add</button>
+      <form>
+        <h2>Add item</h2>
+        <input type="text" />
+        <button>Add</button>
+      </form>
     </>
   );
 }
