9174af5e02e311a288b730241b8149a8c14d7010
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-08-19 20:09:03 +0200

    Add some more tasks to component testing

--- src/__tests__/component_testing/1_Paragraph.tsx
+++ src/__tests__/component_testing/1_Paragraph.tsx
@@ -7,7 +7,6 @@ import { render } from "@testing-library/react";
 // 1. Render a paragraph with some text in it with the `render`-function from @testing-library-react.
 // 2. Use getByText to check that element contains the given text.
 // 3. Check that the element returned by getByText is rendered in the document.
-
 test("renders a paragraph with the text 'Hello world'", () => {
   const { getByText } = render(<></>);
   // const paragraphElement = getByText();
