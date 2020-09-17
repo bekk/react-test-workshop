963fd6a0d3a30d5d909d1df768513db393cc3662
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-09-17 13:08:46 +0200

    Updates

---
+++ precache-manifest.15c04c1555823df0a915d4ec34bd035b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d697f1732ee00aa94ace7318cea346c",
+    "url": "/react-test-workshop%20/index.html"
+  },
+  {
+    "revision": "adbd3104674c33b02e1a",
+    "url": "/react-test-workshop%20/static/css/main.6eae6919.chunk.css"
+  },
+  {
+    "revision": "8260f8f4a8b868869f85",
+    "url": "/react-test-workshop%20/static/js/2.74437e08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-workshop%20/static/js/2.74437e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbd3104674c33b02e1a",
+    "url": "/react-test-workshop%20/static/js/main.f5cb1e0a.chunk.js"
+  },
+  {
+    "revision": "75b36ba7e8a930299c26",
+    "url": "/react-test-workshop%20/static/js/runtime-main.5f21682c.js"
+  }
+]);
