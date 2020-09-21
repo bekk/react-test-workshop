cf349649f539d114e56895a3e78c263f30042ea8
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-09-22 00:35:03 +0200

    Make test modul ready for exercises

--- src/__tests__/mocking/completion-utils-fetchmock-tests.ts
+++ src/__tests__/mocking/completion-utils-fetchmock-tests.ts
@@ -4,10 +4,12 @@ import fetchMock from "fetch-mock";
 describe("Tests for getCompletion() function", () => {
   // Oppgave 3a
   test("getCompletionRate() computes completion rate in percent based on nb of created vs nb of deleted todos", async () => {
-    // use fetchmock.get() here ...
+    // 💡 her skal du bruke fetchmock.get() ...
+    // 💡 kommenter ut disse to linjer under og kjør testen
     // const completion = await getCompletionRate();
     // expect(completion).toBe(50);
   });
 
   // Oppgave 3b
+  // 💡 skriv en ny test her ...
 });
